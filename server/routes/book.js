e35cb61d46848871331243a339d1a5a14ebd0412
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-02-12 14:53:47 +0200

    a

--- server/routes/book.js
+++ server/routes/book.js
@@ -2,24 +2,19 @@ var express = require('express');
 var router = express.Router();
 const Books = require('../models/Books');
 
-
-router.post('/api/book', async function(req, res, next) {
+router.post('/api/book', async function(req, res) {
   console.log("posting")
   const name = req.body.name;
   const author = req.body.author;
   const pages = req.body.pages;
-  try {
-    const book = new Books({
-      name: name,
-      author: author,
-      pages: pages
-    });
-    await book.save();
-    res.status(200).json(book);
-  } catch(error) {
-    res.status(500).json({ error: 'Error occurred when book!' });
-  }
 
+  const book = new Books({
+    name: name,
+    author: author,
+    pages: pages
+  });
+  book.save();
+  res.status(200).json("NaM");
 });
 
 module.exports = router;
