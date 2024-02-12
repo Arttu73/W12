765813e2249ec6c8ec27c5766fe059ea9b663466
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-02-12 15:58:09 +0200

    a

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/book', booksRouter);           
+//app.use('/api/book', booksRouter);           
 
 if (process.env.NODE_ENV === "development") {
     var corsOptions = {
