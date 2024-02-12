f1d107a36671302ee8c0e061dcaeee34d9dd7c25
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-02-12 14:18:30 +0200

    ?

--- server/routes/book.js
+++ server/routes/book.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 const Books = require('../models/Books');
 
-/* GET home page. */
+
 router.post('/api/book', async function(req, res, next) {
+  console.log("posting")
   const name = req.body.name;
   const author = req.body.author;
   const pages = req.body.pages;
